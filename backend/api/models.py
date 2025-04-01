16fbff1238ec5c84903385640b918ea396bdc3ed
AmelkaS <amelkasoszynska@interia.pl>
2025-04-01 15:38:30 +0200

    feat(api): add Reservation model

--- backend/api/models.py
+++ backend/api/models.py
@@ -22,3 +22,13 @@ class Equipment(models.Model):
 
     def __str__(self):
         return f"{self.name} ({self.quantity})"
+    
+class Reservation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.user} → {self.room} ({self.start_time} - {self.end_time})"
+
